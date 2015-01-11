799608a3b61d0c46095ab9c8c5ee2b8bdc868f1c
R. Ian Lee, Sr <ian@houseoflees.net>
2015-01-11 00:31:59 -0600

    Adding code that was used in the making of the video for my blog post.

---
+++ MoveAServo/MoveAServo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+using Gadgeteer.Modules.Gadgeteerin;
+
+namespace MoveAServo
+{
+    public partial class Program
+    {
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            /*******************************************************************************************
+            Modules added in the Program.gadgeteer designer view are used by typing 
+            their name followed by a period, e.g.  button.  or  camera.
+            
+            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
+                button.ButtonPressed +=<tab><tab>
+            
+            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
+                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
+                timer.Tick +=<tab><tab>
+                timer.Start();
+            *******************************************************************************************/
+
+
+            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
+            Debug.Print("Program Started");
+
+            var timer = new GT.Timer(25);
+            timer.Tick += t =>
+            {
+                var v = (float)(potentiometer.ReadPotentiometerPercentage()*100.0);
+                servoTidy.MovePercent(v);
+            };
+            timer.Start();
+        }
+    }
+}
---
+++ Pedometer/Pedometer/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Math = System.Math;
+
+namespace Pedometer
+{
+    public partial class Program
+    {
+        // This method is run when the mainboard is powered up or reset.   
+        void ProgramStarted()
+        {
+            Debug.Print("Program Started");
+
+            new Thread(WatchForSteps).Start();
+        }
+
+        void WatchForSteps()
+        {
+            const int WINDOW = 100;
+            var queue = new double[WINDOW];
+            double total = 0;
+            var currentIdx = 0;
+            int x, y, z;
+            double mag;
+
+            const double MAG_THRESH = 300;
+            var isAbove = false;
+            var stepCount = 0;
+
+            while (true)
+            {
+                accelG248.GetXYZ(out x, out y, out z);
+                mag = Math.Sqrt(x*x + y*y + z*y);
+                if(Double.IsNaN(mag)) continue;
+                
+                // High pass filter
+                total -= queue[currentIdx];
+                queue[currentIdx] = mag;
+                total += mag;
+                currentIdx++;
+                if (currentIdx == WINDOW)
+                {
+                    currentIdx = 0;
+                }
+                //var mag1 = mag;
+                mag = total/WINDOW;
+                //Debug.Print(x + " | " + y + " | " + z + " | " + mag1 + " | " + mag);
+
+                // Zero crossing
+                if (isAbove)
+                {
+                    if (mag >= MAG_THRESH) continue;
+
+                    isAbove = false;
+                    stepCount++;
+                    var outIdx = stepCount/7.15;
+                    for (var j = 0; j <= 6; j++)
+                    {
+                        ledStrip.SetLED(j, j < outIdx);
+                    }
+                }
+                else
+                {
+                    isAbove = mag > MAG_THRESH;
+                }
+            }
+        }
+    }
+}
